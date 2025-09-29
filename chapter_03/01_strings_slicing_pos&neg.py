a88d2b9222b2496ceb9ac69db844361a9f8fe72e
Ahmed Ali <ahmedyarr1212@gmail.com>
2025-09-30 00:09:42 +0500

    chapter 3

---
+++ chapter_03/01_strings_slicing_pos&neg.py
@@ -0,0 +1,16 @@
+a = "Ahmed"
+##    0 1 2 3 4
+##   -5-4-3-2-1
+
+
+# print(a[0:3]) # output 'Ahm' # postive slicing # [0:3] means start from 0 index until 3 (excluding 3)
+# print(a[0:3]) # output 'Ahm' # postive slicing # [0:3]
+print(a[-4:-3]) # output '' # negative slicing # [-4:-3] -> [3:4]
+print(a[3:4]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[-1]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[-2]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[-3]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[-4]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[-5]) # output '' # negative slicing # [-4:-3] -> [3:4]
+# print(a[0:]) # 'Ahmed' # [0:3] means start from 0 index until 3 (excluding 3)
+# print(a[:5]) # 'Ahmed' # [0:3] means start from 0 index until 3 (excluding 3)
---
+++ chapter_03/02_str_functions.py
@@ -0,0 +1,9 @@
+a  = "Ahmed are"
+
+print(len(a))
+print(a.endswith("ed"))
+print(a.startswith("Ah"))
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+print(a.casefold())
